f78eea496ee230ba679a6bb015cb375784c75834
shanmukhnimmagadda <41318663+shanmukhnimmagadda@users.noreply.github.com>
2018-07-18 07:41:12 +0000

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,5 @@
-string=raw_input("Enter string:")
-vowels=0
-for i in string:
-      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
-            vowels=vowels+1
-print("Number of vowels are:")
-print(vowels)
+pp= raw_input()
+if pp in ('a','e','i','o','u'):
+print("vowel")
+esle:
+	print("consonant")
