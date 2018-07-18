c6145fa1e048b6c2b4807384e4cef49e8555b938
shanmukhnimmagadda <41318663+shanmukhnimmagadda@users.noreply.github.com>
2018-07-18 05:33:55 +0000

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,5 +1,5 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
+nu = int(raw_input())
+if (nu % 2) == 0:
+   print("Even")
 else:
-   print("{0} is Odd".format(num))
+   print("Odd")
